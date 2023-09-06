eceb0dc2b83eaef4bb0c3099be12713c7be6d1b8
yohannkallyl <kallylyohann@gmail.com>
2023-09-06 17:01:44 -0300

    ajuste de cor no footer

--- ex016 exercício/script.js
+++ ex016 exercício/script.js
@@ -11,6 +11,7 @@ if(fano.value.length == 0 || fano.value > ano ){
 
 
 } else{
+    var cor = document.getElementById("cor")
     var def = document.getElementById("definir")
     var fsex = document.getElementsByName("radsex")
     var idade = ano - Number(fano.value)
@@ -30,18 +31,22 @@ if(fano.value.length == 0 || fano.value > ano ){
             img.setAttribute("src", "bebe-homem.jpg")
             document.body.style.background = "#ffd9b6"
             def.innerHTML = "CRIANÇA"
+            cor.style.color = "black"
         } else if (idade < 21){
             img.setAttribute("src", "jovem-homem.jpg")
             document.body.style.background = "#211c22"
             def.innerHTML = "JOVEM"
+            cor.style.color = "white"
         } else if(idade < 50){
             img.setAttribute("src", "adulto-homem.jpg")
             document.body.style.background = "#275076"
             def.innerHTML = "ADULTO"
+            cor.style.color = "white"
         } else{
             img.setAttribute("src", "velho-homem.jpg")
             document.body.style.background = "#4d382e"
             def.innerHTML = "IDOSO"
+            cor.style.color = "white"
         }
 
     
@@ -52,19 +57,23 @@ if(fano.value.length == 0 || fano.value > ano ){
             img.setAttribute("src", "bebe-mulher.jpg")
             document.body.style.background = "#ffd9b6"
             def.innerHTML = "CRIANÇA"
+            cor.style.color = "black"
         } else if (idade < 21){
             img.setAttribute("src", "jovem-mulher.jpg")
             document.body.style.background = "#a00400"
             def.innerHTML = "JOVEM"
+            cor.style.color = "white"
             
         } else if (idade < 50){
             img.setAttribute("src", "adulto-mulher.jpg")
             document.body.style.background = "#362b38"
             def.innerHTML = "ADULTA"
+            cor.style.color = "white"
         } else{
             img.setAttribute("src", "velha-mulher.jpg")
             document.body.style.background = "#ecede8"
             def.innerHTML = "IDOSA"
+            cor.style.color = "black"
         }
     }
 
