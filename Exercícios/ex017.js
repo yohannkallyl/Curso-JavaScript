b2336aefbd18ccd34fb780f766dd32a48305e15f
yohannkallyl <kallylyohann@gmail.com>
2023-09-07 16:05:15 -0300

    update

---
+++ Exercícios/ex017.js
@@ -0,0 +1,21 @@
+var c = 1
+while (c <= 2){
+    console.log(`Passo ${c}`)
+    c++ // mesma coisa que c+1, ou seja, o valor de c da variável vai aumentando de 1 em 1 
+}
+
+
+
+/* também podemos usar só que com o TESTE LÓGICO NO FINAL. Por ter o teste no final, ela garante que esse código
+rodará pelo menos uma vez*/
+
+
+/*
+var c = 1
+   do{
+    console.log(`Passo ${}`)
+    c++
+   } while (c <= 6)
+   
+   
+   */
